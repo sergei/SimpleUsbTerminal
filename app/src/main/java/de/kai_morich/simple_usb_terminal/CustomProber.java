c97578208740b20981d5cb761b2d4789e86ec775
Sergei <spodshivalov@scmarinetech.com>
2022-12-26 12:18:26 -0800

    Add Yacht Devices YDNU NMEA2000 USB Gateway VID and PID

--- app/src/main/java/de/kai_morich/simple_usb_terminal/CustomProber.java
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/CustomProber.java
@@ -15,6 +15,8 @@ class CustomProber {
     static UsbSerialProber getCustomProber() {
         ProbeTable customTable = new ProbeTable();
         customTable.addProduct(0x16d0, 0x087e, CdcAcmSerialDriver.class); // e.g. Digispark CDC
+        customTable.addProduct(1155, 41495, CdcAcmSerialDriver.class);   // (Yacht Devices Ltd.)
+
         return new UsbSerialProber(customTable);
     }
 
